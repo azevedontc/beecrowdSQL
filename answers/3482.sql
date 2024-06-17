1d0d325399892261070c154109108a970dd129f0
Azevedo <ubaporangacity@gmail.com>
2024-06-17 19:07:18 -0300

    3482

--- answers/3482.sql
+++ answers/3482.sql
@@ -1,13 +1,21 @@
 SELECT
-    LEAST(u1.user_name, u2.user_name) AS u1_name,
-    GREATEST(u1.user_name, u2.user_name) AS u2_name
+    CASE 
+        WHEN u1.posts < u2.posts THEN u1.user_name
+        ELSE u2.user_name
+    END AS u1_name,
+    CASE 
+        WHEN u1.posts < u2.posts THEN u2.user_name
+        ELSE u1.user_name
+    END AS u2_name
 FROM
     followers f1
-    JOIN followers f2 ON f1.user_id_fk = f2.following_user_id_fk
+    JOIN followers f2 ON f1.user_id_fk = f2.following_user_id_fk AND f1.following_user_id_fk = f2.user_id_fk
     JOIN users u1 ON f1.user_id_fk = u1.user_id
-    JOIN users u2 ON f2.following_user_id_fk = u2.user_id
+    JOIN users u2 ON f1.following_user_id_fk = u2.user_id
 WHERE
-    f1.following_user_id_fk = f2.user_id_fk
-    AND u1.posts < u2.posts
+    u1.user_id < u2.user_id
 ORDER BY
-    u1.user_id;
+    CASE 
+        WHEN u1.posts < u2.posts THEN u1.user_id
+        ELSE u2.user_id
+    END;
